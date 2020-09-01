4db9f99f603caffbaf932a7a91e3159dfedb2a31
bhmmam@gmial.com <bhmmam@gmial.com>
2020-09-01 17:17:07 +0200

    GET NEW NUMBERS WHEN  PLAYERS INPUTS RIGHT ANSWER

--- JSCode/game-engine.js
+++ JSCode/game-engine.js
@@ -1,10 +1,19 @@
-let tempNum1 = generateNumber();
-let tempNum2 = generateNumber();
-let tempOp =  pickOperator();
-let num1 = document.querySelector("#num1").innerHTML = Math.max(tempNum1, tempNum2);
-let num2 = document.querySelector("#num2").innerHTML = Math.min(tempNum1, tempNum2);
-let opertor = document.querySelector("#operator").innerHTML = tempOp;
-let checkBtn = document.querySelector(".btn-input");
+window.addEventListener("load",init);
+
+const num1 = document.querySelector("#num1");
+const num2 = document.querySelector("#num2");
+const opertor = document.querySelector("#operator");
+const checkBtn = document.querySelector(".btn-input");
+const result = document.querySelector("#result")
+
+function init(){
+    let temp1 = generateNumber();
+    let temp2 = generateNumber();
+    num1.innerHTML= Math.max(temp1,temp2);
+    num2.innerHTML= Math.min(temp1,temp2);
+    opertor.innerHTML=pickOperator();
+}
+
 
 function generateNumber(){
     return Math.round(Math.random() * 49)+1;
@@ -16,8 +25,8 @@ function pickOperator(){
 }
 
 checkBtn.addEventListener("click",()=>{
-    let resultValue = document.querySelector("#result").value;
-    alert(eval(tempNum1+opertor+tempNum2)==resultValue);
+    if(eval(num1.innerHTML+opertor.innerHTML+num2.innerHTML)==result.value){
+        init();
+        result.value=""
+    }
 });
-
-
