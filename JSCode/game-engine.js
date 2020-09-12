8efee49fccb78f52cdc3a149cb76a0f2ea3d15d6
Hmmam Babi Khouja <bhmmam@gmail.com>
2020-09-12 11:47:23 +0200

    MAKE THE UI MORE FLUENT
    1 I made the browser foucs on the text field when it loads
    
    2 I made the check button get triggered by ENTER

--- JSCode/game-engine.js
+++ JSCode/game-engine.js
@@ -1,24 +1,31 @@
+/*TODO:
+XD do the enter trigger
+XD make the value text field is where the focus when the page loads or where the player win/ lose the round 
+* add it to my portfolio 
+*/
+
 window.addEventListener("load",init);
 
 /* vars */
 const levels = {
-    "unli" :{ opertors:["+","-","*","/"],maxNum :999 },
-    "kg" :{ opertors:["+","-"],maxNum :5 },
-    "c1" :{ opertors:["+","-"],maxNum :50 },
-    "c2" :{ opertors:["+","-"],maxNum :100 }
+    // "unli" :{ operators:["+","-","*","/"],maxNum :999 },
+    "kg" :{ operators:["+","-"],maxNum :5 },
+    "c1" :{ operators:["+","-"],maxNum :50 },
+    "c2" :{ operators:["+","-"],maxNum :100 }
 }
 
-let opertors = levels.unli.opertors;// default
-let maxNum = levels.unli.maxNum;// default
+let operators = levels.kg.operators;// default
+let maxNum = levels.kg.maxNum;// default
 let score = 0 ;
 
 /* DOM vars */
 const num1 = document.querySelector("#num1");
 const num2 = document.querySelector("#num2");
-const opertor = document.querySelector("#operator");
+const operator = document.querySelector("#operator");
 const checkBtn = document.querySelector(".btn-input");
-const result = document.querySelector("#result")
-const scoreDisplay = document.querySelector(".socre-value");
+const result = document.querySelector("#result");
+result.focus();
+const scoreDisplay = document.querySelector(".score-value");
 scoreDisplay.innerHTML=score;
 const levelSelector = document.querySelector("#levels");
 levelSelector.addEventListener("change", setLevel, false);
@@ -28,7 +35,7 @@ function init(){
     let temp2 = generateNumber();
     num1.innerHTML= Math.max(temp1,temp2);
     num2.innerHTML= Math.min(temp1,temp2);
-    opertor.innerHTML=pickOperator();
+    operator.innerHTML=pickOperator();
 }
 
 function generateNumber(){
@@ -36,14 +43,14 @@ function generateNumber(){
 }
 
 function pickOperator(){
-    return opertors[Math.floor(Math.random()*opertors.length)];
+    return operators[Math.floor(Math.random()*operators.length)];
 }
 
-checkBtn.addEventListener("click",()=>{
+function enterVal(){
     checkBtn.classList.add("btn-input-clicked")
     setTimeout(()=>{checkBtn.classList.remove("btn-input-clicked")}, 400); 
 
-    if(eval(num1.innerHTML+opertor.innerHTML+num2.innerHTML)==result.value){
+    if(eval(num1.innerHTML+operator.innerHTML+num2.innerHTML)==result.value){
         score+=1;
     }else{
         score = 0;
@@ -51,12 +58,12 @@ checkBtn.addEventListener("click",()=>{
     scoreDisplay.innerHTML=score;
     init();
     result.value=""
-});
+};
 
 function setLevel(){
-    let temp = levelSelector.value;
-    opertors = levels[temp].opertors;// default
-    maxNum = levels[temp].maxNum;
+    let level = levelSelector.value;
+    operators = levels[level].operators;// default
+    maxNum = levels[level].maxNum;
     init();
     score = 0 ;
     scoreDisplay.innerHTML=score;
