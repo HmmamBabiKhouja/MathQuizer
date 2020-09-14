eb9c241a13ca34c47f7e24e36d639257ad5ae4af
Hmmam Babi Khouja <bhmmam@gmail.com>
2020-09-14 17:52:50 +0200

    FIXING INPUT ISSUE
    when check is pressed it would the value even if it is null or ""
    
    to fix it I added validation if block with warning so user enter value

--- JSCode/game-engine.js
+++ JSCode/game-engine.js
@@ -1,22 +1,29 @@
-/*TODO:
-XD do the enter trigger
-XD make the value text field is where the focus when the page loads or where the player win/ lose the round 
-* add it to my portfolio 
-*/
+/*FIX:
+ D don't take the result value if it's empty 
+ * redo evaluation of each level
+ */
 
-window.addEventListener("load",init);
+window.addEventListener("load", init);
 
 /* vars */
 const levels = {
-    // "unli" :{ operators:["+","-","*","/"],maxNum :999 },
-    "kg" :{ operators:["+","-"],maxNum :5 },
-    "c1" :{ operators:["+","-"],maxNum :50 },
-    "c2" :{ operators:["+","-"],maxNum :100 }
+    "kg": {
+        operators: ["+", "-"],
+        maxNum: 5
+    },
+    "c1": {
+        operators: ["+", "-"],
+        maxNum: 50
+    },
+    "c2": {
+        operators: ["+", "-"],
+        maxNum: 100
+    }
 }
 
-let operators = levels.kg.operators;// default
-let maxNum = levels.kg.maxNum;// default
-let score = 0 ;
+let operators = levels.kg.operators; // default
+let maxNum = levels.kg.maxNum; // default
+let score = 0;
 
 /* DOM vars */
 const num1 = document.querySelector("#num1");
@@ -26,45 +33,51 @@ const checkBtn = document.querySelector(".btn-input");
 const result = document.querySelector("#result");
 result.focus();
 const scoreDisplay = document.querySelector(".score-value");
-scoreDisplay.innerHTML=score;
+scoreDisplay.innerHTML = score;
 const levelSelector = document.querySelector("#levels");
 levelSelector.addEventListener("change", setLevel, false);
 
-function init(){
+function init() {
     let temp1 = generateNumber();
     let temp2 = generateNumber();
-    num1.innerHTML= Math.max(temp1,temp2);
-    num2.innerHTML= Math.min(temp1,temp2);
-    operator.innerHTML=pickOperator();
+    num1.innerHTML = Math.max(temp1, temp2);
+    num2.innerHTML = Math.min(temp1, temp2);
+    operator.innerHTML = pickOperator();
 }
 
-function generateNumber(){
-    return Math.round(Math.random() * maxNum)+1;
+function generateNumber() {
+    return Math.round(Math.random() * maxNum) + 1;
 }
 
-function pickOperator(){
-    return operators[Math.floor(Math.random()*operators.length)];
+function pickOperator() {
+    return operators[Math.floor(Math.random() * operators.length)];
 }
 
-function enterVal(){
+function enterVal() {
     checkBtn.classList.add("btn-input-clicked")
-    setTimeout(()=>{checkBtn.classList.remove("btn-input-clicked")}, 400); 
-
-    if(eval(num1.innerHTML+operator.innerHTML+num2.innerHTML)==result.value){
-        score+=1;
+    setTimeout(() => {
+        checkBtn.classList.remove("btn-input-clicked")
+    }, 400);
+    if (result.value === null || result.value === "") {
+        alert("Enter the result of the equation instead of the ? before you hit check")
     }else{
-        score = 0;
+        if (eval(num1.innerHTML + operator.innerHTML + num2.innerHTML) == result.value) {
+            score += 1;
+        } else {
+            score = 0;
+        }
+        scoreDisplay.innerHTML = score;
+        init();
+        result.value = ""
     }
-    scoreDisplay.innerHTML=score;
-    init();
-    result.value=""
+  
 };
 
-function setLevel(){
+function setLevel() {
     let level = levelSelector.value;
-    operators = levels[level].operators;// default
+    operators = levels[level].operators; // default
     maxNum = levels[level].maxNum;
     init();
-    score = 0 ;
-    scoreDisplay.innerHTML=score;
+    score = 0;
+    scoreDisplay.innerHTML = score;
 }
